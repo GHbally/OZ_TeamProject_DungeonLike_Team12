bc8da858c75b995c1af57766c77d9b6e820e8718
ma-cilg <cilg0208@gmail.com>
2026-06-19 16:57:53 +0900

    Player_heal
    add Player_healSystem

--- Assets/Project/07. Scripts/Monster/ExpOrb/ExpOrb.cs
+++ Assets/Project/07. Scripts/Monster/ExpOrb/ExpOrb.cs
@@ -5,17 +5,28 @@ public class ExpOrb : MonoBehaviour
     [Header("경험치 설정")]
     public int expAmount = 10;
 
+    private PlayerLevelSystem playerLevelSystem;
+
+    private void Start()
+    {
+        PlayerBase player = FindFirstObjectByType<PlayerBase>();
+
+        if (player != null)
+        {
+            playerLevelSystem = player.GetComponent<PlayerLevelSystem>();
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // 플레이어만 먹기 가능
-        if (!other.CompareTag("Player"))
-            return;
+        //플레이어만 먹기 가능
+        if (!other.CompareTag("Player")) return;
 
-        Debug.Log($"경험치 {expAmount} 획득");
-
-        // 나중에 경험치 시스템 연결
-        // other.GetComponent<PlayerExp>()
-        //      .AddExp(expAmount);
+        //경험치 획득
+        if (playerLevelSystem != null)
+        {
+            playerLevelSystem.EarnExp(expAmount);
+            Debug.Log($"경험치 {expAmount} 획득");
+        }
 
         ReturnPool();
     }
