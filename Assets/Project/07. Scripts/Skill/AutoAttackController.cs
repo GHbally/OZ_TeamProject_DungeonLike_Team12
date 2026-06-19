356fc0182b21f36a4e44216b20621807444aeec8
ma-cilg <cilg0208@gmail.com>
2026-06-19 14:24:03 +0900

    skill scripts
    add Comment

--- Assets/Project/07. Scripts/Skill/AutoAttackController.cs
+++ Assets/Project/07. Scripts/Skill/AutoAttackController.cs
@@ -44,6 +44,8 @@ public class AutoAttackController : MonoBehaviour
             targetDetector = GetComponent<TargetDetector>();
         }
     }
+
+    //공격
     private void TryAttack()
     {
         Transform target = targetDetector.FindNearestTarget(attackStats.AttackRange);
@@ -55,19 +57,23 @@ public class AutoAttackController : MonoBehaviour
 
         attackTimer = attackStats.GetAttackInterval();
     }
+
+    //발사 메서드(파이어포인트에서)
     private void FireProjectile(Transform target)
     {
         if (target == null)
         {
             return;
         }
+
+        //(적 위치 좌표 - 내 총구 위치 좌표) 계산해서 발사 방향 계산
         Vector2 direction = (target.position - firePoint.position).normalized;
 
         Debug.Log(
         $"[AutoAttack] 사용하는 풀: " +
         $"{(projectilePool == null ? "NULL" : projectilePool.gameObject.name)}",
         gameObject
-    );
+        );
 
         DamageInfo1 damageInfo = attackStats.CreateDamageInfo(gameObject);
 
@@ -87,10 +93,14 @@ public class AutoAttackController : MonoBehaviour
             attackTimer = 0f;
         }
     }
+
+    //공격 멈추기
     public void StopAttack()
     {
         SetAttackEnabled(false);
     }
+
+    //다시 공격
     public void ResumeAttack()
     {
         SetAttackEnabled(true);
