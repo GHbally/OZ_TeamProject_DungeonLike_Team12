ede529e003afa76cc55a3a0e11eb83c0ff350e6f
suan <tndkssla@gmail.com>
2026-06-24 18:12:58 +0900

    Update WaveData.cs

--- Assets/Project/07. Scripts/Monster/Wave/WaveData.cs
+++ Assets/Project/07. Scripts/Monster/Wave/WaveData.cs
@@ -5,4 +5,9 @@ public class WaveData
 {
     public int warriorCount;
     public int archerCount;
+
+    //몬스터 체력 배율
+    public float hpMultiplier = 1f;
+    //몬스터 이동속도 배율
+    public float speedMultiplier = 1f;
 }
