1f40dbfeb6038fc380320c76e04226335d916e9d
ma-cilg <cilg0208@gmail.com>
2026-06-26 14:34:12 +0900

    Player FIx
    fixed dash arrow

--- Assets/Project/07. Scripts/Player/PlayerMovement.cs
+++ Assets/Project/07. Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("이동속도")]
-    [SerializeField] private float moveSpeed = 5.0f; //이동 속도
+    [SerializeField] private float moveSpeed = 5.0f;    //이동 속도
+
+    [Header("사운드 설정")]
+    private AudioSource audioSource;                    //소리 재생해 줄 컴포넌트
+    [SerializeField] private AudioClip dashSound;       //인스펙터에서 넣을 대쉬 오디오
 
     //[읽기, 쓰기] 외부에서 내 MoveSpeed를 수정할 수 있게 열기
     //이동 속도는 외부적인 스펙업을 통해 바뀔 여지가 있으므로 읽기+쓰기
@@ -46,6 +50,9 @@ public class PlayerMovement : MonoBehaviour
             animator = visualTransform.GetComponentInChildren<Animator>();
         }
         playerBase = GetComponent<PlayerBase>();
+
+        //플레이어 본체에 붙어있는 AudioSource 컴포넌트 가져오기
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -118,6 +125,12 @@ public class PlayerMovement : MonoBehaviour
     {
         canDash = false;    //대쉬 썼으니 쿨타임 상태로
         isDashing = true;   //대쉬 중 켜기
+
+        //대쉬 효과음
+        if (audioSource != null && dashSound != null)
+        {
+            audioSource.PlayOneShot(dashSound);
+        }
 
         //대쉬 도중 방향 고정
         dashDirection = moveVec;
